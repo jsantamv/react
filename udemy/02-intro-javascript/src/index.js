9793458702908a868a0f7ffbdcfd442bc15847c1
Juan Carlos Santamaria <jsantamaria@namutek.com>
2021-02-13 22:01:35 -0600

    Actualizacion de fuente

---
+++ udemy/02-intro-javascript/src/index.js
@@ -0,0 +1,17 @@
+
+
+
+const nombre = 'hola mudno'
+
+let valordado = 5
+
+console.log(nombre,valordado)
+
+if (true){
+    let valordado = 4
+    console.log(valordado)
+}
+
+console.log(valordado)
+
+
