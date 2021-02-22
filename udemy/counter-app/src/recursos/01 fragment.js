98650c364adc06997e5dddfd12e022efab0f6793
Juan Carlos Santamaria <jsantamaria@namutek.com>
2021-02-21 20:59:04 -0600

    creacion de componentes

---
+++ udemy/counter-app/src/recursos/01 fragment.js
@@ -0,0 +1,21 @@
+//import React, { Fragment } from 'react';
+import React from 'react';
+
+/**
+ * <> fragmente en forma corta
+ */
+
+
+ 
+const PrimeraApp = () => {
+
+        return (
+            <>
+                <h1>Hola mundo</h1>
+                <p>afd asdf asdfas df</p>
+             </>
+        );
+
+}
+
+export default PrimeraApp;
