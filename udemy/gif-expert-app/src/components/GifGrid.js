cb5db74c19b47b337ce5a32425bf5fba5387cc97
Juan Carlos Santamaria <jsantamaria@namutek.com>
2021-04-25 20:13:24 -0600

    Compresion de uso de los HOOKS

--- udemy/gif-expert-app/src/components/GifGrid.js
+++ udemy/gif-expert-app/src/components/GifGrid.js
@@ -1,35 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGif'
 import { GifGridItem } from './GifGridItem'
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setimages] = useState([])
-
-    //para que se renderize una unica vez
-    useEffect(() => {
-        getFGifts()
-    }, [])
-
-    const getFGifts = async () => {
-        const url = 'https://api.giphy.com/v1/gifs/search?api_key=IrJwy71enEpDaAVanUv2AWa7QamoITgN&q=Rick and morthy&limit=100'
-
-        const resp = await fetch(url)
-        const { data } = await resp.json();
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-        console.log(gifs);
-        setimages(gifs)
-    }
+    const { data: images, loading } = useFetchGifs(category)
 
     return (
         <>
-            <h3> {category} </h3>
-            <div className='card-grid'>
+            <h3 className='animate__animated animate__bounceInUp'> {category} </h3>
+
+            {loading && <p className='animate__animated animate__flash'>Cargando...</p>}
+
+            {<div className='card-grid '>
                 {
                     images.map(img => (
                         <GifGridItem
@@ -38,7 +21,7 @@ export const GifGrid = ({ category }) => {
                         />
                     ))
                 }
-            </div>
+            </div>}
         </>
     )
 }
